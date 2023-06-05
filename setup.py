340ca52ab529632bd37c0a2643da0d1203756ab1
Victor Westerlund <victor.vesterlund@gmail.com>
2023-06-05 14:26:03 +0200

    fix: setup config (#4)

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup, find_packages
 
 setup(
     name = "reflect-client",
-    version = "1.0.1",
+    version = "1.1.1",
     description = "Python library for communicating with an API built with Reflect over HTTP or UNIX sockets",
     author = "Victor Westerlund",
     author_email = "victor.vesterlund@gmail.com",
     url = "https://github.com/victorwesterlund/reflect-client-python",
     packages = ["reflect_client"],
     # The requirement.txt file should also be updated when changing this
-    install_requires = ["validators"]
+    install_requires = ["validators", "requests"]
 )
